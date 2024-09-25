5e052aed49ee806685732d2f7c86531dca7e42a2
Nicolas Pepin-Perreault <nicolas.pepin-perreault@camunda.com>
2024-09-25 23:29:05 +0200

    feat: add job metric exporter to auto scale job workers

---
+++ demo/exporter/src/main/java/com/github/sijoma/exporter/JobCountMetadata.java
@@ -0,0 +1,137 @@
+package com.github.sijoma.exporter;
+
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import java.nio.ByteOrder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import net.jcip.annotations.ThreadSafe;
+import org.agrona.BufferUtil;
+import org.agrona.collections.MutableInteger;
+import org.agrona.concurrent.UnsafeBuffer;
+
+@ThreadSafe
+final class JobCountMetadata {
+  private static final ByteOrder ENDIANNESS = ByteOrder.LITTLE_ENDIAN;
+  private static final Charset CHARSET = StandardCharsets.UTF_8;
+  private final ConcurrentMap<String, Integer> counts = new ConcurrentHashMap<>();
+  private final MeterRegistry meterRegistry;
+
+  JobCountMetadata(MeterRegistry meterRegistry) {
+    this.meterRegistry = meterRegistry;
+  }
+
+  public void increment(final String jobType) {
+    final var actual = counts.get(jobType);
+    if (actual == null) {
+      counts.put(jobType, 1);
+      monitorJobType(jobType);
+    } else {
+      counts.put(jobType, actual + 1);
+    }
+  }
+
+  public void decrement(final String jobType, final int count) {
+    final var actual = counts.get(jobType);
+    if (actual <= count) {
+      counts.remove(jobType);
+    }
+
+    counts.computeIfPresent(
+        jobType,
+        (key, value) -> {
+          final var newValue = value - count;
+          if (newValue < 0) {
+            return null;
+          }
+          return newValue <= 0 ? null : newValue;
+        });
+  }
+
+  public void clear() {
+    counts.clear();
+  }
+
+  public byte[] serialize() {
+    final var length = length();
+
+    if (length == 0) {
+      return BufferUtil.NULL_BYTES;
+    }
+
+    final var bytes = new byte[length];
+    final var buffer = new UnsafeBuffer(bytes);
+    final var offset = new MutableInteger(0);
+    counts.forEach(
+        (type, count) -> offset.value += serializeCount(buffer, offset.value, type, count));
+
+    assert offset.value == length : "expected total bytes serialized to equal predicted length";
+    return bytes;
+  }
+
+  public void deserialize(final byte[] bytes) {
+    final var buffer = new UnsafeBuffer(bytes);
+    counts.clear();
+
+    if (bytes.length == 0) {
+      return;
+    }
+
+    int offset = 0;
+    while (offset < bytes.length) {
+      final var type = buffer.getStringUtf8(offset, ENDIANNESS);
+      offset += Integer.BYTES + getStringLength(type);
+
+      final var count = buffer.getInt(offset, ENDIANNESS);
+      offset += Integer.BYTES;
+
+      counts.put(type, count);
+    }
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    JobCountMetadata that = (JobCountMetadata) o;
+    return Objects.equals(counts, that.counts);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(counts);
+  }
+
+  @Override
+  public String toString() {
+    return "JobCountMetadata{" + "counts=" + counts + '}';
+  }
+
+  private int serializeCount(
+      final UnsafeBuffer buffer, final int offset, final String type, final int count) {
+    int bytesWritten = buffer.putStringUtf8(offset, type, ENDIANNESS);
+    buffer.putInt(offset + bytesWritten, count, ENDIANNESS);
+    return bytesWritten + Integer.BYTES;
+  }
+
+  private int length() {
+    final var length = new MutableInteger(0);
+    counts.forEach((type, ignored) -> length.value += 2 * Integer.BYTES + getStringLength(type));
+    return length.value;
+  }
+
+  private int getStringLength(final String type) {
+    return type.getBytes(CHARSET).length;
+  }
+
+  private void monitorJobType(final String jobType) {
+    Gauge.builder("zeebe.jobs.available", () -> counts.getOrDefault(jobType, 0))
+        .baseUnit("job")
+        .tag("jobType", jobType)
+        .description("Count of available jobs per type")
+        .register(meterRegistry);
+  }
+}
---
+++ demo/exporter/src/main/java/com/github/sijoma/exporter/JobMetricExporter.java
@@ -0,0 +1,89 @@
+package com.github.sijoma.exporter;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import io.camunda.zeebe.exporter.api.Exporter;
+import io.camunda.zeebe.exporter.api.context.Context;
+import io.camunda.zeebe.exporter.api.context.Controller;
+import io.camunda.zeebe.protocol.record.Record;
+import io.camunda.zeebe.protocol.record.intent.IncidentIntent;
+import io.camunda.zeebe.protocol.record.intent.JobBatchIntent;
+import io.camunda.zeebe.protocol.record.intent.JobIntent;
+import io.camunda.zeebe.protocol.record.value.IncidentRecordValue;
+import io.camunda.zeebe.protocol.record.value.JobBatchRecordValue;
+import io.camunda.zeebe.protocol.record.value.JobRecordValue;
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+
+public final class JobMetricExporter implements Exporter {
+  private final CompositeMeterRegistry meterRegistry = new CompositeMeterRegistry();
+  private final JobCountMetadata jobCounters = new JobCountMetadata(meterRegistry);
+  private final Cache<Long, String> keyToTypeCache =
+      Caffeine.newBuilder().maximumSize(10_000).build();
+
+  private Controller controller;
+
+  @Override
+  public void configure(final Context context) throws Exception {
+    meterRegistry
+        .add(context.getMeterRegistry())
+        .config()
+        .commonTags("partitionId", String.valueOf(context.getPartitionId()));
+  }
+
+  @Override
+  public void open(final Controller controller) {
+    this.controller = controller;
+    controller.readMetadata().ifPresent(jobCounters::deserialize);
+  }
+
+  @Override
+  public void close() {
+    meterRegistry.close();
+    jobCounters.clear();
+  }
+
+  @SuppressWarnings("unchecked")
+  @Override
+  public void export(final Record<?> record) {
+    if (record.getIntent() instanceof JobBatchIntent intent) {
+      final var jobRecord = (Record<JobBatchRecordValue>) record;
+      if (intent == JobBatchIntent.ACTIVATED) {
+        jobCounters.decrement(
+            jobRecord.getValue().getType(), jobRecord.getValue().getJobs().size());
+      }
+    } else if (record.getIntent() instanceof JobIntent intent) {
+      final var jobRecord = (Record<JobRecordValue>) record;
+      final var type = (jobRecord).getValue().getType();
+      switch (intent) {
+        case CREATED:
+          keyToTypeCache.put(jobRecord.getKey(), type);
+        case TIMED_OUT:
+        case YIELDED:
+        case RECURRED_AFTER_BACKOFF:
+          jobCounters.increment(type);
+          break;
+        case FAILED:
+          // on failed, only increment if we would retry immediately; otherwise wait for the recur
+          // event
+          if (jobRecord.getValue().getRetries() > 0
+              && jobRecord.getValue().getRetryBackoff() <= 0) {
+            jobCounters.increment(type);
+          }
+          break;
+        default:
+          break;
+      }
+    } else if (record.getIntent() instanceof IncidentIntent intent) {
+      final var incidentRecord = (Record<IncidentRecordValue>) record;
+      final var jobKey = incidentRecord.getValue().getJobKey();
+      if (intent == IncidentIntent.RESOLVED && jobKey > 0) {
+        final var jobType = keyToTypeCache.get(jobKey, ignored -> null);
+        if (jobType != null) {
+          jobCounters.increment(jobType);
+        }
+      }
+    }
+
+    controller.updateLastExportedRecordPosition(record.getPosition(), jobCounters.serialize());
+  }
+}
